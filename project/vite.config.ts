44296c2482e9db80adb606ca53563e0b84b37056
mohamed elaraby <mohamedalarbic6.com>
2025-02-27 23:50:39 +0200

    second commit

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001, // Change this to your preferred port
+  },
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
