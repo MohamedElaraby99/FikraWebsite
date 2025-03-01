2c9ce78a97dd7ce83a59cb41271c2aa32610e8ce
mohamed elaraby <mohamedalarbic6.com>
2025-03-01 18:38:02 +0200

    23

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -196,10 +196,13 @@ function App() {
   ];
 
   const socialLinks = [
-    { id: 1, icon: "Instagram", url: "https://instagram.com" },
-    { id: 2, icon: "Facebook", url: "https://facebook.com" },
-    { id: 3, icon: "Mail", url: "mailto:softwarefikra@gmail.com" },
-    { id: 4, icon: "Phone", url: "tel:+201207039410" },
+    {
+      id: 1,
+      icon: "Facebook",
+      url: "https://www.facebook.com/share/1AN2gA2bMq/",
+    },
+    { id: 2, icon: "Mail", url: "mailto:softwarefikra@gmail.com" },
+    { id: 3, icon: "Phone", url: "tel:+201207039410" },
   ];
 
   const partners = [
